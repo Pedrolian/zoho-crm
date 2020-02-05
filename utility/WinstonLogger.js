027e08dc10031311d0864b9e8f101e492185c92e
Pedrolian <pedrodvasconcelos@gmail.com>
2020-02-05 08:47:21 -0300

    Added console transport as default Winston Transport.

--- utility/WinstonLogger.js
+++ utility/WinstonLogger.js
@@ -2,7 +2,19 @@ const winston = require('winston');
 const path = require('path');
 
 const logger = winston.createLogger({
-  transports: []
+  transports: [
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+      format: winston.format.combine(
+        winston.format.label({ label: path.basename(process.mainModule.filename) }),
+        winston.format.colorize(),
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.printf(
+          info => `(${info.timestamp})[${info.label}]-[${info.level}]: ${info.message}`
+        )
+      )
+    })
+  ]
 });
 
 setPath = (logDirectory) => {
@@ -27,20 +39,6 @@ setPath = (logDirectory) => {
       level: 'info',
       format: winston.format.combine(
         winston.format.label({ label: path.basename(process.mainModule.filename) }),
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.printf(
-          info => `(${info.timestamp})[${info.label}]-[${info.level}]: ${info.message}`
-        )
-      )
-    })
-  );
-
-  logger.add(
-    new winston.transports.Console({
-      level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
-      format: winston.format.combine(
-        winston.format.label({ label: path.basename(process.mainModule.filename) }),
-        winston.format.colorize(),
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         winston.format.printf(
           info => `(${info.timestamp})[${info.label}]-[${info.level}]: ${info.message}`
