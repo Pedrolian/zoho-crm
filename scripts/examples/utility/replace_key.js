6c8544d72d852c6c3d53cee7e95baf61549179c0
Pedrolian <pedrodvasconcelos@gmail.com>
2020-02-04 21:45:00 -0300

    Removed examples, @Pedrolian/ImportZoho-Example

--- scripts/examples/utility/replace_key.js
+++
@@ -1,8 +0,0 @@
-// Parse examples
-const ReplaceKey = require('../../../utility/ReplaceKey.js');
-
-const test = ReplaceKey.exchange([{ "id": "00T1Q00003vNfrqUAC" },{ "id": "00T1Q00003vO47JUAS" }],  { "id": "ID", "description": "desc" }, "new");
-console.table(test);
-
-const test2 = ReplaceKey.exchange([{ "id": "00T1Q00003vNfrqUAC" },{ "id": "00T1Q00003vO47JUAS" }],  { "id": "ID", "description": "desc" }, ["new","check"]);
-console.table(test2);
