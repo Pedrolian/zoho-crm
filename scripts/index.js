67ca012a8da5cc08c2170a4b47ef499cfa87191d
Pedrolian <pedrodvasconcelos@gmail.com>
2020-09-03 17:30:00 -0300

    Fix: scripts index respects WINSTON_LOG_PATH instead of putting log one folder back.

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,11 @@ require("dotenv").config({ path: "../.env" });
 
 // Initiate logs
 Logger = require("../utility/WinstonLogger.js");
-Logger.setPath("../log");
+Logger.setPath(process.env.WINSTON_LOG_PATH || "../log");
 
 // Initiate Stack Class
 const StackClass = require("../class/StackClass.js");
-const Stack = new StackClass(
-  process.env.STACK_POOL_SIZE ? process.env.STACK_POOL_SIZE : 5,
-  process.env.STACK_MAX_ATTEMPTS ? process.env.STACK_MAX_ATTEMPTS : 10,
-  process.env.STACK_DELAY ? process.env.STACK_DELAY : 100
-);
+const Stack = new StackClass(process.env.STACK_POOL_SIZE ? process.env.STACK_POOL_SIZE : 5, process.env.STACK_MAX_ATTEMPTS ? process.env.STACK_MAX_ATTEMPTS : 10, process.env.STACK_DELAY ? process.env.STACK_DELAY : 100);
 
 // Initiate Zoho Class
 const Zoho = require("../class/ZohoClass.js");
