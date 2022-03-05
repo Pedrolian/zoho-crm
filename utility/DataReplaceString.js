5a029a68afec3bf51dbcd048357cffb2f5cb768e
Pedrolian <Pedrodvasconcelos@gmail.com>
2022-03-04 23:24:04 -0300

    Feat: Criteria now is clean before searching API

--- utility/DataReplaceString.js
+++ utility/DataReplaceString.js
@@ -1,12 +1,14 @@
 const removeDiacritics = require('./RemoveDiactric.js');
 
+const cleanCriteria = (val) => encodeURIComponent(val.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/(?=[(),])/g, '\\').replace(/\(/g, "%28").replace(/\)/g, "%29"))
+
 module.exports.replace = function (data, str, skipDiacritics) {
   skipDiacritics = skipDiacritics || false;
   let tmpStr = str;
-  if(str.match(/\$(\w+)/g) != null)
+  if(str.match(/\$_(\w+)/g) != null)
   {
-    str.match(/\$(\w+)/g).map(key => {
-      tmpStr = tmpStr.replace(key, data[key.replace("$_", "")]);
+    str.match(/\$_(\w+)/g).map(key => {
+      tmpStr = tmpStr.replace(key, cleanCriteria(data[key.replace("$_", "")]));
     });
   }
   return skipDiacritics ? tmpStr : removeDiacritics.replace(tmpStr);
