98d27ff9d74ada121f0ab0c1215feb929334c7eb
Pedrolian <Pedrodvasconcelos@gmail.com>
2022-03-23 11:18:01 -0300

    Fix: Validate before replacing criteria string

--- utility/DataReplaceString.js
+++ utility/DataReplaceString.js
@@ -3,12 +3,18 @@ const removeDiacritics = require('./RemoveDiactric.js');
 const cleanCriteria = (val) => encodeURIComponent(val.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/(?=[(),])/g, '\\').replace(/\(/g, "%28").replace(/\)/g, "%29"))
 
 module.exports.replace = function (data, str, skipDiacritics) {
+
+  if(data === null || data === undefined) return str;
+  if(data.constructor === Object && Object.keys(data).length === 0) return str;
+
   skipDiacritics = skipDiacritics || false;
   let tmpStr = str;
   if(str.match(/\$_(\w+)/g) != null)
   {
     str.match(/\$_(\w+)/g).map(key => {
-      tmpStr = tmpStr.replace(key, cleanCriteria(data[key.replace("$_", "")]));
+      if(data.hasOwnProperty(key.replace("$_", ""))) {
+        tmpStr = tmpStr.replace(key, cleanCriteria(data[key.replace("$_", "")]));
+      }
     });
   }
   return skipDiacritics ? tmpStr : removeDiacritics.replace(tmpStr);
